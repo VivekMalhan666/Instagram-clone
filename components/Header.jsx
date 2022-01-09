6ee88dc216399957c900239d477eb9d4a73110a1
fsociety <fsociety@root>
2022-01-09 13:56:47 +0530

    Sign In - Design sign in page - redirect post login - logout on user click - display options on login

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,10 +7,11 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from '@heroicons/react/outline';
-
 import { HomeIcon } from '@heroicons/react/outline';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 function Header() {
+  const { data: session } = useSession();
   return (
     <div className="shadow-sm border-b bg-white sticky top-0 z-50">
       <div className="flex justify-between mx-5 lg:mx-auto bg-white max-w-6xl ">
@@ -50,21 +51,27 @@ function Header() {
         <div className="flex items-center justify-end space-x-4">
           <HomeIcon className="navBtn" />
           <MenuIcon className="h-6 md:hidden cursor-pointer" />
-          <div className=" relative navBtn">
-            <div className="absolute -top-1 -right-2 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white text-xs w-5 h-5">
-              3
-            </div>
-            <PaperAirplaneIcon className="navBtn rotate-45" />
-          </div>
-          <PlusCircleIcon className="navBtn" />
-          <UserGroupIcon className="navBtn" />
-          <HeartIcon className="navBtn" />
-
-          <img
-            src="https://media.pri.org/s3fs-public/styles/story_main/public/story/images/mr.%20robot.jpg?itok=4iHmKmck"
-            alt="profile pic"
-            className="h-10 rounded-full cursor-pointer w-10 object-cover"
-          />
+          {session ? (
+            <>
+              <div className=" relative navBtn">
+                <div className="absolute -top-1 -right-2 bg-red-500 rounded-full flex items-center justify-center animate-pulse text-white text-xs w-5 h-5">
+                  3
+                </div>
+                <PaperAirplaneIcon className="navBtn rotate-45" />
+              </div>
+              <PlusCircleIcon className="navBtn" />
+              <UserGroupIcon className="navBtn" />
+              <HeartIcon className="navBtn" />
+              <img
+                src={session.user.image}
+                onClick={signOut}
+                alt="profile pic"
+                className="h-10 rounded-full cursor-pointer w-10 object-cover"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign In</button>
+          )}
         </div>
       </div>
     </div>
--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,17 +1,36 @@
+import dynamic from 'next/dynamic';
 import { getProviders, signIn } from 'next-auth/react';
 
+const Header = dynamic(() => import('../../components/Header'));
+
 function SignIn({ providers }) {
-  console.log(providers);
   return (
     <>
-      {providers &&
-        Object.values(providers).map((provider) => (
-          <div key={provider.name}>
-            <button onClick={() => signIn(provider.id)}>
-              Sign in with {provider.name}
-            </button>
-          </div>
-        ))}
+      <Header />
+      <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
+        <img
+          className="w-80 object-contain"
+          src="https://links.papareact.com/ocw"
+          alt="Instagam logo"
+        />
+        <p className="text-sm italic">
+          This is not the REAL instagram app, It is built for educational
+          purposes only
+        </p>
+        <div className="mt-40">
+          {providers &&
+            Object.values(providers).map((provider) => (
+              <div key={provider.name}>
+                <button
+                  className="p-3 bg-blue-500 rounded-lg text-white"
+                  onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+                >
+                  Sign in with {provider.name}
+                </button>
+              </div>
+            ))}
+        </div>
+      </div>
     </>
   );
 }
